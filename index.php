48cd0e75b07593ed1e11befd04e66bd11ecacb45
DenisY3rk2 <39835145+DenisY3rk2@users.noreply.github.com>
2021-07-11 22:50:17 -0500

    menu v0.1

--- index.php
+++ index.php
@@ -11,6 +11,31 @@
 </head>
 
 <body>
+    <header>
+    <nav class="dropdown-menu">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="./">Logo</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="./">Inicio</a></li>
+            <li class="active"><a href="./">Municipio</a></li>
+            <li class="active"><a href="./">Noticias</a></li>
+            <li class="active"><a href="./">Trámite documentario</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Pag1<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                <li><a href="www.google.com">Listado 1</a></li>
+                <li><a href="vistas/hola.php">Listado 2</a></li>
+                <li><a href="vistas/hola.php">Listado 3</a></li>
+                </ul>
+            </li>
+         
+        </ul>
+    </div>
+    </nav>
+    
+</header>
 
 </body>
 </html>
