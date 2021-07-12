428d088b1623986572846578f8504414959bb33f
DenisY3rk2 <39835145+DenisY3rk2@users.noreply.github.com>
2021-07-12 00:26:58 -0500

    menu v3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Municipalidad Distrital de San Juan del Oro</title>
     <link rel="stylesheet" href="vistas/css/bootstrap.css">
     <link rel="stylesheet" href="vistas/css/bootstrap-theme.css">
@@ -14,21 +14,31 @@
 <body>
     <body>
     <header>
-      <nav>
-        <ul id="menu">
-          <li><a href="">Logo</a></li> 
-          <li><a href="">Inicio</a></li>
-          <li><a href="">Municipio</a>
-            <ul>
-              <li><a href="">Visión</a></li>
-              <li><a href="">Misión</a></li>
-              <li><a href="">Organigrama</a></li>
-            </ul>  
-          </li>
-          <li><a href="">Noticias</a></li>
-          <li><a href="">Trámite documentario</a></li>
-        </ul>
-      </nav>
+      <div class="header-content">
+          <div class="logo">
+              <img src="vistas/img/logo1.png">
+          </div>
+      
+          <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Municipio</a>
+                        <ul>
+                            <li><a href="#">Misión</a></li>
+                            <li><a href="#">Visión</a></li>
+                            <li><a href="#">Organigrama</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">Noticias</a></li>
+                    <li><a href="#">Trámite documentario</a></li> 
+                </ul>
+            </nav>
+
+        
+          </div>
+      </div>
     </header>
   </body>
 </html>
