bc0e2ed67efb39125c29d89717bf18cdaf62ed1c
DenisY3rk2 <39835145+DenisY3rk2@users.noreply.github.com>
2021-07-11 22:47:36 -0500

    menu1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Municipalidad Distrital de San Juan del Oro</title>
+    <link rel="stylesheet" href="vistas/css/bootstrap.css">
+    <link rel="stylesheet" href="vistas/css/bootstrap-theme.css">
+    <script src="vistas/js/bootstrap.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
